61d4e4180e98e0ea97462fa69659ce24e400a449
Sadanon0 <sadanon.pr.66@ubu.ac.th>
2026-08-31 18:29:47 +0700

    ส่งงาน31สิงหา
    หีบ+ประตู

---
+++ Assets/Scrips/Chest Interaction.cs
@@ -0,0 +1,19 @@
+using UnityEditor.Scripting.LifecycleManagement;
+using UnityEngine;
+
+public class ChestInteraction : MonoBehaviour, IInteractable
+{
+    Animator anim;
+    bool opened;
+
+    void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
+    public void Interact()
+    {
+        if (!opened) anim.SetTrigger("open");
+        else anim.SetTrigger("close");
+        opened = !opened;
+    }
+}
